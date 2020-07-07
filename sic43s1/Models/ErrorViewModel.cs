99ff54c8e34712819f62bc5a1a6609293f2783f0
pichaya <pichaya@sic.co.th>
2020-07-07 11:39:27 +0700

    - sic43s1 web demonstation cmac calculate - used bouncycastle for calculated cmac

---
+++ sic43s1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sic43s1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
